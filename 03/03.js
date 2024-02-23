8cd70b2e14008d0319112b55b62c078728ff9b6f
kyung89 <bamtori1029@gmail.com>
2024-02-23 15:38:29 +0900

    0223 03.html 버튼 실습 코드

--- 03/03.js
+++ 03/03.js
@@ -1,3 +1,31 @@
+// DOM TREE 가 완성된 후 실행
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded ok"); // 웹 콘솔에 출력
+
+    // 버튼 생성
+    const bt3 = document.createElement("button");
+    const bt3Txt = document.createTextNode("버튼3");
+    bt3.appendChild(bt3Txt);
+    bt3.setAttribute("id", "bt3"); // id 속성 달기
+
+    const bt4 = document.createElement("button");
+    const bt4Txt = document.createTextNode("버튼4");
+    bt4.appendChild(bt4Txt);
+    bt4.setAttribute("id", "bt4");
+
+    // 버튼 추가하기
+    document.querySelector("#btArea2").append(bt3);
+    document.querySelector("#btArea2").append(bt4);
+    
+    // 버튼 이벤트 달기
+    bt3.addEventListener("click", ()=>{
+        handleClick(3);
+    });
+    bt4.addEventListener("click", ()=>{
+        handleClick(4);
+    });
+});
+
 /*
  * 함수 작성 2: 화살표 함수
  */
@@ -7,8 +35,10 @@
 const handleClick = (n) => {
 
     let msg;
-    if (n == 1) msg = "<h2>안녕 하세요</h2>";
-    else msg = "<h2>안녕히 가세요</h2>";
+    if (n == 1) msg = "<h2>안녕하세요</h2>";
+    else if (n == 2) msg = "<h2>안녕히 가세요</h2>";
+    else if (n == 3) msg = `<h2>버튼 ${n}</h2>`;
+    else if (n == 4) msg = `<h2>버튼 ${n}</h2>`;
     document.querySelector("#msgArea").innerHTML = msg;
     // callback 함수를 쓸 때 사용할 예정
 }
